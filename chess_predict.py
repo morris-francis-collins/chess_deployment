cd966fa8d2468645037810652a4f8ed5d30ead82
morris-francis-collins <137597562+morris-francis-collins@users.noreply.github.com>
2023-10-27 22:45:23 -0700

    First commit from GitHub

--- chess_predict.py
+++ chess_predict.py
@@ -117,7 +117,7 @@ def chess_predict(pgn):
 # 2312       0       2723       2626    97            27
 
 # Winner 1
-pgn = "1.d4 Nf6 2.Nf3 g6 3.c4 Bg7 4.Nc3 d6 5.e4 c6 6.Be2 O-O 7.O-O Qe8 8.e5 Nfd7 9.exd6 exd6 10.Bf4 Qe6 11.Re1 Rd8 12.Bd3 Qg4 13.Bxd6 Nf6 14.c5 Bf5 15.h3 Qh5 16.g4 Nxg4 17.Bxf5 gxf5 18.hxg4 fxg4 19.Ne5 f5 20.Qb3+ Kh8 21.Nf7+ Kg8 22.Nxd8+ Kh8 23.Nf7+ Kg8 24.Nh6+ Kh8 25.Qg8#"
+# pgn = "1.d4 Nf6 2.Nf3 g6 3.c4 Bg7 4.Nc3 d6 5.e4 c6 6.Be2 O-O 7.O-O Qe8 8.e5 Nfd7 9.exd6 exd6 10.Bf4 Qe6 11.Re1 Rd8 12.Bd3 Qg4 13.Bxd6 Nf6 14.c5 Bf5 15.h3 Qh5 16.g4 Nxg4 17.Bxf5 gxf5 18.hxg4 fxg4 19.Ne5 f5 20.Qb3+ Kh8 21.Nf7+ Kg8 22.Nxd8+ Kh8 23.Nf7+ Kg8 24.Nh6+ Kh8 25.Qg8#"
 
 # Winner -1
 # pgn = "1.e4 e6 2.d4 d5 3.Nd2 Be7 4.e5 c5 5.Qg4 g6 6.dxc5 Nd7 7.Ndf3 Nxc5 8.Bd3 Qa5+ 9.Bd2 Qb6 10.b4 Nxd3+ 11.cxd3 Bd7 12.a4 h5 13.Qd4 Nh6 14.Ne2 Nf5 15.Qb2 O-O 16.O-O Rfc8 17.h3 Qa6 18.b5 Qb6 19.Ned4 Bc5 20.Nxf5 exf5 21.a5 Qxb5 22.Rfb1 Qxb2 23.Rxb2 Bc6 24.Be3 b6 25.d4 Be7 26.axb6 axb6 27.Rxa8 Rxa8 28.Rxb6 Rc8 29.Bg5 Bf8 30.e6 fxe6 31.Ne5 Be8 32.Rxe6 Kh7 33.Ra6 Bg7 34.Bf6 Bxf6 35.Rxf6 Kg7 36.Rd6 Bf7 37.Rd7 Rf8 38.h4 f4 39.Rb7 Kg8 40.Kf1 Be8 41.Ke2 Rf6 42.Kf3 Kf8 43.Rc7 Ba4 44.Rc5 Bd1#"
